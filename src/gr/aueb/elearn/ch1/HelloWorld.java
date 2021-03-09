17ebf6e52220d19a32a3579d2b4c9299b2f79bb9
phaidon <fedoninho@gmail.com>
2021-03-09 16:25:58 +0200

    Hello World v 0.2

--- src/gr/aueb/elearn/ch1/HelloWorld.java
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -1,5 +1,9 @@
 package gr.aueb.elearn.ch1;
 
+/**
+ * Prints Hello World
+ * @author pgrig
+ */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
